03af0b63b669b5ec7ad9465a817b21b673170893
yukio <yoyuohlhjl@adsg>
2016-04-15 18:31:01 +0900

    tyotto kaetayo

--- test/app/src/main/java/com/example/denjo/test/preStart.java
+++ test/app/src/main/java/com/example/denjo/test/preStart.java
@@ -15,17 +15,7 @@ public class preStart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_start);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void moveTakingpic(View view) {
