5e54be0eea42bf68179599d2fc9dba145cf82a06
yukio <yoyuohlhjl@adsg>
2016-04-18 01:05:52 +0900

    plus some pages(toggle, waiting) but not connected. deleted bar and button of "prestart".

--- test/app/src/main/java/com/example/denjo/test/preStart.java
+++ test/app/src/main/java/com/example/denjo/test/preStart.java
@@ -7,10 +7,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.format.DateFormat;
 import android.view.View;
 
@@ -32,17 +29,6 @@ public class preStart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_start);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
---
+++ test/app/src/main/java/com/example/denjo/test/waiting.java
@@ -0,0 +1,13 @@
+package com.example.denjo.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class waiting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting);
+    }
+}
