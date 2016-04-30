2d5c6da631f3c0d64c8425207b3f2c190de307f6
Iwami kazuya <k.bluewing.i@gmail.com>
2016-04-30 23:22:19 +0900

    fragment→activityに移植

--- test/app/src/main/java/com/example/denjo/test/preStart.java
+++ test/app/src/main/java/com/example/denjo/test/preStart.java
@@ -1,5 +1,6 @@
 package com.example.denjo.test;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -10,7 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.example.denjo.test.utils.HttpRequest;
+import com.example.denjo.test.utils.AsyncCallback;
+import com.example.denjo.test.utils.HttpConnector;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class preStart extends AppCompatActivity {
@@ -22,7 +32,7 @@ public class preStart extends AppCompatActivity {
         setContentView(R.layout.activity_pre_start);
         imageView = (ImageView) findViewById(R.id.imageView);
 
-        HttpRequest httpRequest = new HttpRequest();
+        postImage();
     }
 
     public void onClickCameraButton(View view) {
@@ -43,6 +53,51 @@ public class preStart extends AppCompatActivity {
     public void move(View view) {
         Intent intent = new Intent(this, test.class);
         startActivity(intent);
+    }
+
+    private void postImage(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(!HttpConnector.isConnected(connectivityManager)){
+            new AlertDialog.Builder(this)
+                    .setMessage("インターネット接続できません")
+                    .setPositiveButton("OK", null)
+                    .show();
+            return;
+        }
+
+        HttpConnector.RequestInfo requestInfo = new HttpConnector.RequestInfo();
+        requestInfo.url = "http://writedownurlhere";
+        requestInfo.params.add(new HttpConnector.Param(HttpConnector.Param.TYPE_STRING, "key_param1", "value_param1"));
+        requestInfo.params.add(new HttpConnector.Param(HttpConnector.Param.TYPE_STRING, "key_param2", "value_param2"));
+        requestInfo.asyncCallBack = new AsyncCallback() {
+            @Override
+            public void onPostExecute(InputStream responseIS) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(responseIS));
+                StringBuilder buf = new StringBuilder();
+                String line;
+                try{
+                    while((line = reader.readLine()) != null){
+                        buf.append(line);
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+                    return;
+                }
+                String responseStr = buf.toString();
+
+                JSONObject jsonObject = null;
+                try{
+                    jsonObject = new JSONObject(responseStr);
+                    /* ! */
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+                }
+
+                /*  */
+            }
+        };
+        HttpConnector.Request(this, requestInfo);
     }
 
 }
