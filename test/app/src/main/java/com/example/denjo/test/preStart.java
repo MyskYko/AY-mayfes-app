1db52a8ff0f6601befdd7ed5e7d20306962289dc
yukio <yoyuohlhjl@adsg>
2016-05-12 16:39:00 +0900

    error hyouzi wo tyousetu simasita

--- test/app/src/main/java/com/example/denjo/test/preStart.java
+++ test/app/src/main/java/com/example/denjo/test/preStart.java
@@ -48,8 +48,6 @@ public class preStart extends AppCompatActivity {
             //setContentView(R.layout.activity_waiting);
             postImage(capturedImage);
         }
-        //撮影失敗時
-        setContentView(R.layout.activity_retake);
     }
 
     private void postImage(Bitmap capturedImage){
